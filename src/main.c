274ad50714d4a7df9f81a09f7eb2548f30ca912c
R0-main <justromixx@gmail.com>
2025-04-11 16:15:27 +0200

    yeah working pseudo 3d

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: rguigneb <rguigneb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/28 10:15:20 by rguigneb          #+#    #+#             */
-/*   Updated: 2025/04/11 12:36:00 by rguigneb         ###   ########.fr       */
+/*   Updated: 2025/04/11 16:13:24 by rguigneb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,14 @@
 #define MAP_HEIGHT 7
 #define MAP_WIDTH 10
 #define SIZE 64
-#define FOV 75 * (PI / 180)
+#define FOV 60 * (PI / 180)
 #define MAX_ITERATION 10
 
 #define SCREEN_HEIGHT MAP_HEIGHT *SIZE
 #define SCREEN_WIDTH MAP_WIDTH *SIZE
 
 #define X_SIZE 4
-#define RAYS_COUNT 20 // (int)((SCREEN_WIDTH) / X_SIZE)
+#define RAYS_COUNT (int)((SCREEN_WIDTH) / X_SIZE)
 
 #define PLAYER_SPEED 1
 
@@ -95,13 +95,23 @@ void	on_key_pressed(int key, t_data *data)
 	}
 	if (key == 'a')
 	{
-		data->player.rotation_angle -= 0.1;
+		data->player.rotation_angle -= 0.05;
 	}
 	if (key == 'd')
 	{
-		data->player.rotation_angle += 0.1;
+		data->player.rotation_angle += 0.05;
 	}
 	data->player.rotation_angle = normalize_angle(data->player.rotation_angle);
+}
+
+int	get_distance_from_the_camera(t_data *data)
+{
+	float	half_screen;
+	float	half_fov;
+
+	half_screen = SCREEN_WIDTH / 2;
+	half_fov = FOV / 2;
+	return (half_screen / tan(half_fov));
 }
 
 // t_img *create_re
@@ -179,6 +189,38 @@ void	draw_rect(t_img *buffer, int color, t_uvec2 start, t_uvec2 end)
 	}
 }
 
+void	draw_straight_line(t_img *buffer, int color, int x, int height)
+{
+	int y;
+	int t = (SCREEN_HEIGHT - height) / 2;
+
+	y = t;
+	while (y < SCREEN_HEIGHT - t)
+	{
+		put_pixel_to_buffer(buffer, (t_uvec2){x, y}, color);
+		y++;
+	}
+	y = t;
+	while (y < SCREEN_HEIGHT - t)
+	{
+		put_pixel_to_buffer(buffer, (t_uvec2){x + 1, y}, color);
+		y++;
+	}
+	y = t;
+	while (y < SCREEN_HEIGHT - t)
+	{
+		put_pixel_to_buffer(buffer, (t_uvec2){x + 2, y}, color);
+		y++;
+	}
+	y = t;
+	while (y < SCREEN_HEIGHT - t)
+	{
+		put_pixel_to_buffer(buffer, (t_uvec2){x + 3, y}, color);
+		y++;
+	}
+
+}
+
 void	draw_map(t_img *buffer, char *map)
 {
 	t_uvec2	tmp;
@@ -205,14 +247,9 @@ void	draw_map(t_img *buffer, char *map)
 
 bool	is_wall(char *map, t_vec_2 pos)
 {
-	if (pos.x < 0)
-		pos.x = 0;
-	if (pos.x > SCREEN_WIDTH)
-		pos.x = SCREEN_WIDTH;
-	if (pos.y < 0)
-		pos.y = 0;
-	if (pos.y > SCREEN_WIDTH)
-		pos.y = SCREEN_HEIGHT;
+	if (pos.x < 0 || pos.x >= SCREEN_WIDTH || pos.y < 0
+		|| pos.y >= SCREEN_HEIGHT)
+		return (true);
 	return (map[(pos.y / SIZE) * MAP_WIDTH + (pos.x / SIZE)] == 1);
 }
 
@@ -257,80 +294,75 @@ bool	is_looking_top_right(float angle)
 	return (angle >= PI + PI / 2 && angle <= 2 * PI);
 }
 
-t_uvec2	get_horizontal_colision(t_fvec2 start, char *map, float angle)
+t_uvec2	get_horizontal_colision(t_img *buffer, t_fvec2 start, char *map,
+		float angle)
 {
 	t_uvec2	dist;
 	int		i;
 	t_vec_2	A;
+	t_vec_2	pos_to_check;
+	int		Ya;
 
 	i = 0;
-	dist = (t_uvec2){start.x, start.y};
+	dist = (t_uvec2){0, 0};
 	if (is_looking_bottom(angle))
-		A.y = ((int)start.y / SIZE) * SIZE;
+		A.y = ((int)start.y / SIZE) * SIZE - 1;
 	else
 		A.y = ((int)start.y / SIZE) * SIZE + SIZE;
+	Ya = 0;
+	if (is_looking_bottom(angle))
+		Ya = -SIZE;
+	else
+		Ya = SIZE;
+	A.x = ((A.y - start.y) / tan(angle)) + start.x;
 	while (i < MAX_ITERATION && A.y / SIZE >= 0 && A.y / SIZE <= MAP_HEIGHT)
 	{
-		A.x = ((A.y - start.y) / tan(angle)) + start.x;
-		if (is_looking_bottom(angle) && is_wall(map, (t_vec_2){A.x, A.y
-				- SIZE}))
-			i = MAX_ITERATION;
-		else if (is_wall(map, A))
-			i = MAX_ITERATION;
 		dist.x = A.x - start.x;
 		dist.y = A.y - start.y;
-		if (is_looking_top(angle))
-			A.y += SIZE;
-		else
-			A.y -= SIZE;
+		if (is_wall(map, (t_vec_2){A.x, A.y}))
+			break ;
+		A.x += Ya / tan(angle);
+		A.y += Ya;
 		i++;
 	}
 	dist.x += start.x;
 	dist.y += start.y;
-	return (dist);
+	return ((t_uvec2){A.x, A.y});
 }
 
-t_uvec2	get_vertical_colision(t_fvec2 start, char *map, float angle)
+t_uvec2	get_vertical_colision(t_img *buffer, t_fvec2 start, char *map,
+		float angle)
 {
 	t_uvec2	dist;
 	int		i;
 	t_vec_2	B;
+	int		Xa;
+	int		Ya;
 
 	i = 0;
 	dist = (t_uvec2){start.x, start.y};
 	if (is_looking_left(angle))
-		B.x = ((int)start.x / SIZE) * SIZE;
+		B.x = ((int)start.x / SIZE) * SIZE - 1;
 	else
 		B.x = ((int)start.x / SIZE) * SIZE + SIZE;
+	if (is_looking_left(angle))
+		Xa = -SIZE;
+	else
+		Xa = SIZE;
+	Ya = Xa * tan(angle);
+	B.y = ((int)start.y) + ((int)start.x - B.x) * -tan(angle);
 	while (i < MAX_ITERATION && B.x / SIZE >= 0 && B.x / SIZE <= MAP_WIDTH)
 	{
-		B.y = ((int)start.y) + ((int)start.x - B.x) * -tan(angle);
-		if (is_wall(map, (t_vec_2){B.x - SIZE, B.y}))
-			i = MAX_ITERATION;
-		if (is_wall(map, B))
-			i = MAX_ITERATION;
 		dist.x = B.x - start.x;
 		dist.y = B.y - start.y;
-		if (is_looking_left(angle))
-		{
-			B.x -= SIZE;
-		}
-		else
-		{
-			B.x += SIZE;
-		}
+		if (is_wall(map, B))
+			break ;
+		B.x += Xa;
+		B.y += Ya;
 		i++;
 	}
 	dist.x += start.x;
 	dist.y += start.y;
-	// if (dist.x < 0)
-	// dist.x = 0;
-	// if (dist.x > SCREEN_WIDTH)
-	// 	dist.x = SCREEN_WIDTH;
-	// if (dist.y < 0)
-	// 	dist.y = 0;
-	// if (dist.y > SCREEN_HEIGHT)
-	// 	dist.y = SCREEN_HEIGHT;
 	return (dist);
 }
 
@@ -412,12 +444,17 @@ int	distance_between(t_uvec2 vec1, t_uvec2 vec2)
 }
 
 void	draw_ray(t_img *buffer, t_player player, t_data *data, float angle,
-		int len)
+		int i)
 {
 	t_uvec2	vert;
 	t_uvec2	hori;
 	t_uvec2	pos;
+	float	height;
+	float	start;
+	float	distance;
+	float	r;
 
+	// ((unsigned int *)buffer->data)[y * (buffer->size_line / 4) + x]
 	// int	Bx;
 	// int	By;
 	// if (angle >= PI / 2 && angle <= PI + PI / 2)
@@ -428,8 +465,14 @@ void	draw_ray(t_img *buffer, t_player player, t_data *data, float angle,
 	// * tan(angle);
 	// distX = (Bx * SIZE) - player.position.x;
 	// distY = By - player.position.y;
-	vert = get_vertical_colision(player.position, data->map, angle);
-	hori = get_horizontal_colision(player.position, data->map, angle);
+	vert = get_vertical_colision(data->rendering_buffer, player.position,
+			data->map, angle);
+	hori = get_horizontal_colision(data->rendering_buffer, player.position,
+			data->map, angle);
+	// draw_rect(buffer, 0xDDDD00, (t_uvec2){vert.x - 2, vert.y - 2},
+	// 	(t_uvec2){vert.x + 2, vert.y + 2});
+	// draw_rect(buffer, 0x0000DD, (t_uvec2){hori.x - 2, hori.y - 2},
+	// 	(t_uvec2){hori.x + 2, hori.y + 2});
 	if (distance_between((t_uvec2){(int)player.position.x,
 			(int)player.position.y},
 			hori) < distance_between((t_uvec2){(int)player.position.x,
@@ -437,12 +480,25 @@ void	draw_ray(t_img *buffer, t_player player, t_data *data, float angle,
 		pos = hori;
 	else
 		pos = vert;
-	draw_line(buffer, 0xFF0000, (t_uvec2){(int)player.position.x,
-		(int)player.position.y}, (t_uvec2)pos);
+	// printf("dist from hor %d\n",
+	// 	distance_between((t_uvec2){(int)player.position.x,
+	// 		(int)player.position.y}, hori));
+	// printf("dist from vert %d\n",
+	// 	distance_between((t_uvec2){(int)player.position.x,
+	// 		(int)player.position.y}, vert));
+	// printf("choosen : %d %d\n", pos.x, pos.y);
 	// draw_line(buffer, 0x00DD00, (t_uvec2){(int)player.position.x,
 	// 	(int)player.position.y}, (t_uvec2)hori);
 	// draw_line(buffer, 0x0000DD, (t_uvec2){(int)player.position.x,
-	// 		(int)player.position.y}, (t_uvec2)vert);
+	// 	(int)player.position.y}, (t_uvec2)vert);
+	distance = distance_between((t_uvec2){(int)player.position.x,
+			(int)player.position.y}, pos);
+	r = get_distance_from_the_camera(data);
+	// draw_line(buffer, 0xFF0000, (t_uvec2){(int)player.position.x,
+	// 	(int)player.position.y}, (t_uvec2)pos);
+	height = (32 / distance) * r;
+	start = (SCREEN_HEIGHT / 2) - (height / 2);
+	draw_straight_line(buffer, 0x00DD00, i, height);
 	// draw_line(buffer, 0x0DD000, (t_uvec2){player.position.x,
 	// player.position.y},
 	// 	(t_uvec2){player.position.x + cos(angle) * len, player.position.y
@@ -458,22 +514,24 @@ void	raycaster(t_img *buffer, t_player player, float angle, int len)
 
 void	draw_player(t_img *buffer, t_data *data, t_player player)
 {
-	float	i;
+	int		i;
 	float	angle;
+	int		x;
 
-	draw_rect(buffer, 0xFF0000, (t_uvec2){((int)player.position.x) - 5,
-		((int)player.position.y) - 5}, (t_uvec2){((int)player.position.x) + 5,
-		((int)player.position.y) + 5});
-	raycaster(buffer, player, player.rotation_angle, 150);
-	// printf("rotaton %02f\n", player.rotation_angle * PI * 180);
-	draw_ray(buffer, player, data, player.rotation_angle, 50);
-	// draw_ray(buffer, player, player.rotation_angle + 0.1, 50);
+	// draw_rect(buffer, 0xFF0000, (t_uvec2){((int)player.position.x) - 5,
+	// 	((int)player.position.y) - 5}, (t_uvec2){((int)player.position.x) + 5,
+	// 	((int)player.position.y) + 5});
+	// raycaster(buffer, player, player.rotation_angle, 15);
+	// // printf("rotaton %02f\n", player.rotation_angle * PI * 180);
+	// draw_ray(buffer, player, data, player.rotation_angle, SCREEN_WIDTH / 2);
 	i = 0;
+	x = 0;
 	angle = player.rotation_angle - (FOV / 2);
 	while (i < RAYS_COUNT)
 	{
-		draw_ray(buffer, player, data, normalize_angle(angle), 25);
+		draw_ray(buffer, player, data, normalize_angle(angle), x);
 		i += 1;
+		x += X_SIZE;
 		angle += FOV / RAYS_COUNT;
 		// i++;
 	}
@@ -486,15 +544,18 @@ void	loop(t_data *data)
 {
 	usleep(10000);
 	mlx_destroy_image(data->mlx, data->rendering_buffer);
+	// printf("distance : %d\n", get_distance_from_the_camera(data));
 	data->rendering_buffer = mlx_new_image(data->mlx, SCREEN_WIDTH,
 			SCREEN_HEIGHT);
-	draw_line(data->rendering_buffer, 0xFF0000, (t_uvec2){50, 200}, (t_uvec2){0,
-		0});
-	draw_line(data->rendering_buffer, 0xDD00DD, (t_uvec2){0, 0}, (t_uvec2){50,
-		90});
-	draw_rect(data->rendering_buffer, 0x00DF0F, (t_uvec2){90, 90},
-		(t_uvec2){120, 99});
-	draw_map(data->rendering_buffer, data->map);
+	// draw_line(data->rendering_buffer, 0xFF0000, (t_uvec2){50, 200},
+	// (t_uvec2){0,
+	// 	0});
+	// draw_line(data->rendering_buffer, 0xDD00DD, (t_uvec2){0, 0},
+	// (t_uvec2){50,
+	// 	90});
+	// draw_rect(data->rendering_buffer, 0x00DF0F, (t_uvec2){90, 90},
+	// 	(t_uvec2){120, 99});
+	// draw_map(data->rendering_buffer, data->map);
 	draw_player(data->rendering_buffer, data, data->player);
 	mlx_put_image_to_window(data->mlx, data->win, data->rendering_buffer, 0, 0);
 }
